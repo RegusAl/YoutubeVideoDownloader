dbc896ccdb58db566ba58023fc1b7a4db17ffdd9
RegusAl <regusal@pm.me>
2023-02-14 15:37:21 +0200

    added audio download option

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import tkinter
+from pytube import YouTube
 import customtkinter
 
-from pytube import YouTube
-
-def startDownload():
+def startDownloadHigh():
     try:
         youtubeLink = link.get()
         youtubeObject = YouTube(youtubeLink, on_progress_callback=onProgress)
-        video = youtubeObject.streams.get_highest_resolution()
         title.configure(text=youtubeObject.title, text_color="white")
         author.configure(text="Uploaded by " + youtubeObject.author, text_color="white")
         length.configure(text=str(youtubeObject.length) + " seconds", text_color="white")
         finishLabel.configure(text="")
+        video = youtubeObject.streams.get_highest_resolution()
         video.download()
+        finishLabel.configure(text="Downloaded", text_color="green")
+    except:
+        finishLabel.configure(text="Download Error!", text_color="red")
+
+def startDownloadAudio():
+    try:
+        youtubeLink = link.get()
+        youtubeObject = YouTube(youtubeLink, on_progress_callback=onProgress)
+        title.configure(text=youtubeObject.title, text_color="white")
+        author.configure(text="Uploaded by " + youtubeObject.author, text_color="white")
+        length.configure(text=str(youtubeObject.length) + " seconds", text_color="white")
+        finishLabel.configure(text="")
+        audio = youtubeObject.streams.get_audio_only()
+        audio.download()
         finishLabel.configure(text="Downloaded", text_color="green")
     except:
         finishLabel.configure(text="Download Error!", text_color="red")
@@ -30,7 +43,7 @@ def onProgress(stream, chunk, bytes_remaining):
 
 # System settings
 customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("blue")
+customtkinter.set_default_color_theme("dark-blue")
 
 # App frame
 app = customtkinter.CTk()
@@ -63,12 +76,18 @@ progressBar.set(0)
 progressBar.pack()
 
 # Download button
-download = customtkinter.CTkButton(app, text="Download", command=startDownload)
-download.pack(padx = 10, pady = 10)
+
+## Button for high resolution download
+download_high_resolution = customtkinter.CTkButton(app, text="Download Highest Resolution", command=startDownloadHigh, height=40, width=150)
+download_high_resolution.pack(padx=10, pady=15)
+
+## Button for mp4 download
+download_audio = customtkinter.CTkButton(app, text=" Download Audio Only (MP4) ", command=startDownloadAudio, height=40, width=150)
+download_audio.pack(padx=10, pady=15)
 
 # Footer
-footer = customtkinter.CTkLabel(app, text = "RegusAl ©2023")
-footer.pack()
+footer = customtkinter.CTkLabel(app, text="RegusAl ©2023")
+footer.pack(pady=30)
 
 # Run app
 app.mainloop()
